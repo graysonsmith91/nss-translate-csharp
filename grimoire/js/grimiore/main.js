8985f6a3d6fbe2873adb4e3718c876a1f41a2fce
Grayson Smith <graysonsmith91@gmail.com>
2023-02-24 13:45:53 -0600

    first 4 exercises complete

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,1 +1,64 @@
-// Put your code here
+console.log("Do you believe in magic?");
+console.log("------------------------");
+
+const GetAllSpells = () => {
+    const allSpells = [
+        {
+            Name: "Turn enemy into a newt",
+            IsEvil: true,
+            EnergyReqired: 5.1
+        },
+        {
+            Name: "Conjure some gold for a local charity",
+            IsEvil: false,
+            EnergyReqired: 2.99
+        },
+        {
+            Name: "Give a deaf person the ability to heal",
+            IsEvil: false,
+            EnergyReqired: 12.2
+        },
+        {
+            Name: "Make yourself emperor of the universe",
+            IsEvil: true,
+            EnergyReqired: 100.0
+        },
+        {
+            Name: "Convince your relatives your political views are correct",
+            IsEvil: false,
+            EnergyReqired: 2921.5
+        }
+    ];
+
+    return allSpells;
+}
+
+const allSpells = GetAllSpells()
+
+const MakeEvilSpellBook = (allSpells) => {
+    const evilBook = {}
+    evilBook.Title = "Evil Book";
+    evilBook.Spells = allSpells.filter(spell => spell.IsEvil)
+    return evilBook;
+}
+
+const MakeGoodSpellBook = (allSpells) => {
+    const goodBook = {}
+    goodBook.Title = "Good Book";
+    goodBook.Spells = allSpells.filter(spell => !spell.IsEvil)
+    return goodBook;
+}
+
+const goodBook = MakeGoodSpellBook(allSpells)
+const evilBook = MakeEvilSpellBook(allSpells)
+
+const DisplaySpellBook = (book) => {
+    console.log(`${book.Title}`);
+    book.Spells.forEach(book => {
+        console.log(`${book.Name}`)
+    });
+}
+
+DisplaySpellBook(goodBook)
+console.log(``)
+DisplaySpellBook(evilBook)
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,82 @@
-// Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const Roll = () => {
+    let dieValue = getRandomInt(1, 6)
+    let dieString = "Unknown";
+
+    switch (dieValue) {
+        case 1:
+            dieString = "one";
+            break;
+        case 2:
+            dieString = "two";
+            break;
+        case 3:
+            dieString = "three";
+            break;
+        case 4:
+            dieString = "four";
+            break;
+        case 5:
+            dieString = "five";
+            break;
+        case 6:
+            dieString = "six";
+            break;
+    }
+
+    const die = {
+        name: dieString,
+        value: dieValue
+    }
+
+    return die;
+
+}
+
+for (let i = 0; i < 10; i++) {
+    let die1 = Roll();
+    let die2 = Roll();
+    let dieTotal = die1.value + die2.value;
+
+    let message = `${die1.name} + ${die2.name} == ${dieTotal}`;
+    if (die1.value == die2.value) {
+        message += " DOUBLES!";
+    }
+
+    console.log(message);
+}
+
+const ToString = () => {
+    let dieString = "Unknown";
+
+    switch (this.Value) {
+        case 1:
+            dieString = "one";
+            break;
+        case 2:
+            dieString = "two";
+            break;
+        case 3:
+            dieString = "three";
+            break;
+        case 4:
+            dieString = "four";
+            break;
+        case 5:
+            dieString = "five";
+            break;
+        case 6:
+            dieString = "six";
+            break;
+    }
+
+    return dieString;
+}
